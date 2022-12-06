94155ba7ab6b3f4361023aa4576c0f08601f844a
0xboredagain <96192463+0xboredagain@users.noreply.github.com>
2022-12-07 01:42:25 +0530

    if statements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+
+    let x = 3;
+
+    if x < 5 {
+        println!("condition was true");
+    } else {
+        println!("condistion was false");
+    }
 }
